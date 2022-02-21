6b0f5ae9a2eca14a4b235cc2948036ef630de73c
Tetiana Legois <mme.tanyalegois@gmail.com>
2022-02-21 15:41:01 +0200

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -6,7 +6,6 @@ import s from './Statistics.module.css';
 const Statistics = ({ title, stats }) => (
   <section className={s.statistics}>
     {title && <h2 className={s.title}>{title}</h2>}
-
     <ul className={s.statList}>
       {stats.map(stat => (
         <li
